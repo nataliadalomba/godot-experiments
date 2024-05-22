0c625f15189d6cd1c079bb46a1ef36434578a215
Natalia DaLomba <natalia@starforce.games>
2024-05-22 12:30:04 -0500

    More progress on SpawnMoney.cs

--- Assets/Scripts/SpawnMoney.cs
+++ Assets/Scripts/SpawnMoney.cs
@@ -2,20 +2,22 @@ using Godot;
 using System;
 
 public partial class SpawnMoney : Button {
-    Sprite2D coins;
+	private const string coinsScenePath = "res://Scenes/CoinsPile.tscn";
+	private Button button;
 
-    // Called when the node enters the scene tree for the first time.
-    public override void _Ready() {
-        coins = SceneFilePath("res://CoinsPile.tscn");
-    }
-
-    // Called every frame. 'delta' is the elapsed time since the previous frame.
-    public override void _Process(double delta) {
-        
-    }
-
-    public bool IsButtonClicked() {
-        if (this.ButtonPressed)
-            
-    }
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready() {
+		button = GetNode<Button>("Button");
+		button.Connect("pressed", new Callable(this, nameof(OnButtonPressed)));
+	}
+	
+	private void OnButtonPressed() {
+		PackedScene coinsScene = (PackedScene) ResourceLoader.Load(coinsScenePath);
+		if (coinsScene != null) {
+			Node2D sceneInstance = (Node2D) coinsScene.Instantiate();
+			//add instantiated scene to the current scene tree as a child of the node running this script
+			sceneInstance.Position = new Vector2(54, -56);
+			AddChild(sceneInstance);
+		} else GD.PrintErr("Failed to load CoinPile Scene!");
+	}
 }
