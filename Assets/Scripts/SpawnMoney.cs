47612f3a405e93ae880118e6dec51650bceac62b
Natalia DaLomba <natalia@starforce.games>
2024-05-22 08:09:31 -0500

    Some tweaks in SpawnMoney.cs

--- Assets/Scripts/SpawnMoney.cs
+++ Assets/Scripts/SpawnMoney.cs
@@ -6,7 +6,7 @@ public partial class SpawnMoney : Button {
 
     // Called when the node enters the scene tree for the first time.
     public override void _Ready() {
-        coins = load("res://CoinsPile.tscn");
+        coins = SceneFilePath("res://CoinsPile.tscn");
     }
 
     // Called every frame. 'delta' is the elapsed time since the previous frame.
