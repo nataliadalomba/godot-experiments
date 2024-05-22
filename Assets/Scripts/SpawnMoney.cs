42c63047c11ca2c9d4024a4119b4c34a9f845064
Natalia DaLomba <natalia@starforce.games>
2024-05-21 21:03:44 -0500

    Set up button with sprite and coin pile sprite. Writing spawn money script

---
+++ Assets/Scripts/SpawnMoney.cs
@@ -0,0 +1,21 @@
+using Godot;
+using System;
+
+public partial class SpawnMoney : Button {
+    Sprite2D coins;
+
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready() {
+        coins = load("res://CoinsPile.tscn");
+    }
+
+    // Called every frame. 'delta' is the elapsed time since the previous frame.
+    public override void _Process(double delta) {
+        
+    }
+
+    public bool IsButtonClicked() {
+        if (this.ButtonPressed)
+            
+    }
+}
