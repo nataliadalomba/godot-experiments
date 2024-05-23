7c3fb06855c4e2a4044bbce7b934ce5454f88b74
Natalia DaLomba <natalia@starforce.games>
2024-05-23 09:18:18 -0500

    Completed SpawnMoney.cs. Coins spawn right above the button and as a child of the button. Used ForceReadableName to have better naming for each instanced Coin Pile

--- Assets/Scripts/SpawnMoney.cs
+++ Assets/Scripts/SpawnMoney.cs
@@ -1,23 +1,20 @@
 using Godot;
-using System;
 
 public partial class SpawnMoney : Button {
-	private const string coinsScenePath = "res://Scenes/CoinsPile.tscn";
-	private Button button;
+	private PackedScene coinPileScene = GD.Load<PackedScene>("res://Scenes/CoinPile.tscn");
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready() {
-		button = GetNode<Button>("Button");
-		button.Connect("pressed", new Callable(this, nameof(OnButtonPressed)));
+		if (coinPileScene == null) {
+			GD.PrintErr(nameof(coinPileScene) + " did not load!");
+			return;
+		}
+		Connect("pressed", new Callable(this, nameof(OnButtonPressed)));
 	}
 	
 	private void OnButtonPressed() {
-		PackedScene coinsScene = (PackedScene) ResourceLoader.Load(coinsScenePath);
-		if (coinsScene != null) {
-			Node2D sceneInstance = (Node2D) coinsScene.Instantiate();
-			//add instantiated scene to the current scene tree as a child of the node running this script
-			sceneInstance.Position = new Vector2(54, -56);
-			AddChild(sceneInstance);
-		} else GD.PrintErr("Failed to load CoinPile Scene!");
+		Node2D coinInstance = (Node2D) coinPileScene.Instantiate();
+		coinInstance.Position = new Vector2(54, -56);
+		AddChild(coinInstance, true);
 	}
 }
